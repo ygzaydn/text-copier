4c78bbd53f392a13c7851203bd0baeaa435bbf70
unknown <ygzaydns@gmail.com>
2023-11-30 21:48:09 +0300

    update

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { translate } from "@vitalets/google-translate-api";
+import { HttpProxyAgent } from "http-proxy-agent";
+import { proxyList } from "@/constant/proxyList";
+const agent = new HttpProxyAgent(
+  "https://" + proxyList[Math.floor(Math.random() * proxyList.length)]
+);
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  const textToTranslate = req.body.text;
+
+  try {
+    const { text } = await translate(textToTranslate, {
+      to: "tr",
+      //fetchOptions: { agent },
+    });
+    res.status(200).json({ text });
+  } catch (err) {
+    res.status(404).send("Error");
+  }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,67 +1,79 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import { useState } from "react";
+import axios from "axios";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-    const [text, setText] = useState("");
-    const [translated, setTranslated] = useState("");
+  const [text, setText] = useState("");
+  const [translated, setTranslated] = useState("");
 
-    return (
-        <main
-            className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className} bg-black`}
-        >
-            <div className="w-full my-4">
-                <h1 className="text-blue-300 text-2xl mb-6">
-                    Metni Yapıştırın
-                </h1>
-                <textarea
-                    className={`w-full bg-slate-50 h-64 text-blue-500 rounded-xl`}
-                    onChange={(e) => setText(e.target.value)}
-                />
-            </div>
+  const translate = async (text) => {
+    try {
+      const textRes = await axios.post(window.location.href + "/api/hello", {
+        text,
+      });
+      setTranslated(textRes.data.text);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-            <div className="w-full flex flex-col my-8">
-                <h1 className="text-blue-300 text-2xl mb-6">
-                    Düzeltilmiş Metin
-                </h1>
-                <textarea
-                    className={`w-full bg-slate-50 h-64 text-blue-500 p-2`}
-                    value={text.replaceAll(" \n", "\n").replaceAll("\n", " ")}
-                    readOnly
-                />
-                <div className="flex space-around mt-2">
-                    <button
-                        disabled={text.length < 1}
-                        onClick={() => {
-                            navigator.clipboard.writeText(
-                                text
-                                    .replaceAll(" \n", "\n")
-                                    .replaceAll("\n", " ")
-                            );
-                            if (text.length > 0) {
-                                alert("Metin Kopyalandı");
-                            }
-                        }}
-                        className="bg-blue-500 px-4 py-2 rounded-sm mx-auto disabled:bg-gray-500 transition-all cursor-pointer disabled:cursor-default text-slate-50"
-                    >
-                        Metni Kopyala
-                    </button>
-                </div>
-            </div>
-            {translated !== "" && (
-                <div className="w-full flex flex-col my-4">
-                    <h1 className="text-blue-300 text-2xl mb-6">
-                        Çevrilmiş Metin
-                    </h1>
-                    <textarea
-                        className={`w-full bg-slate-50 h-64 text-blue-500 p-2`}
-                        value={translated}
-                        readOnly
-                    />
-                </div>
-            )}
-        </main>
-    );
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className} bg-black`}
+    >
+      <div className="w-full my-4">
+        <h1 className="text-blue-300 text-2xl mb-6">Metni Yapıştırın</h1>
+        <textarea
+          className={`w-full bg-slate-50 h-64 text-blue-500 rounded-xl`}
+          onChange={(e) => setText(e.target.value)}
+        />
+      </div>
+
+      <div className="w-full flex flex-col my-8">
+        <h1 className="text-blue-300 text-2xl mb-6">Düzeltilmiş Metin</h1>
+        <textarea
+          className={`w-full bg-slate-50 h-64 text-blue-500 p-2`}
+          value={text.replaceAll(" \n", "\n").replaceAll("\n", " ")}
+          readOnly
+        />
+        <div className="flex space-around mt-2">
+          <button
+            disabled={text.length < 1}
+            onClick={() => {
+              navigator.clipboard.writeText(
+                text.replaceAll(" \n", "\n").replaceAll("\n", " ")
+              );
+              if (text.length > 0) {
+                alert("Metin Kopyalandı");
+              }
+            }}
+            className="bg-blue-500 px-4 py-2 rounded-sm mx-auto disabled:bg-gray-500 transition-all cursor-pointer disabled:cursor-default text-slate-50"
+          >
+            Metni Kopyala
+          </button>
+          <button
+            disabled={text.length < 1}
+            onClick={() => {
+              translate(text.replaceAll(" \n", "\n").replaceAll("\n", " "));
+            }}
+            className="bg-green-500 px-4 py-2 rounded-sm mx-auto disabled:bg-gray-500 transition-all cursor-pointer disabled:cursor-default text-slate-50"
+          >
+            Metni Çevir
+          </button>
+        </div>
+      </div>
+      {translated !== "" && (
+        <div className="w-full flex flex-col my-4">
+          <h1 className="text-blue-300 text-2xl mb-6">Çevrilmiş Metin</h1>
+          <textarea
+            className={`w-full bg-slate-50 h-64 text-blue-500 p-2`}
+            value={translated}
+            readOnly
+          />
+        </div>
+      )}
+    </main>
+  );
 }
