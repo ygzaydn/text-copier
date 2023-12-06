651005381e9fa83c6ac50fe78662c74ed37c79c0
unknown <ygzaydns@gmail.com>
2023-12-06 23:28:19 +0300

    updt

---
+++ _next/static/l9svDToHbKPPnab4chYs5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/840-fb9cd67b217231af.js","static/css/e7d3bab42d9af29d.css","static/chunks/pages/index-10aca3bbaa230eab.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
